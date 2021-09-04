929290b10cb5e7a585b33a125b2654f34e93f7f3
abrie <abrie@users.noreply.github.com>
2021-09-03 20:27:50 -0400

    Learning the API: Extract A0 matrix.

--- app/useeio/main.py
+++ app/useeio/main.py
@@ -1,20 +1,37 @@
 import requests
 import json
+import pandas
+
 
 def load_secrets(f):
-        return json.load(f)
+    return json.load(f)
+
+
+def urljoin(parts):
+    return "/".join(part.strip("/") for part in parts)
+
 
 def run(args):
+    base_url = "https://api.edap-cluster.com/useeio/api/"
     secrets = load_secrets(args.secrets)
-    base_url = 'https://api.edap-cluster.com/useeio/api'
-    #Now use the endpoint to return a list of available models
     headers = {}
-    headers['x-api-key']=secrets["apiKey"]
-    models_response = requests.get(base_url+'/models',headers=headers)
+    headers["x-api-key"] = secrets["apiKey"]
+
+    models_response = requests.get(urljoin([base_url, "models"]), headers=headers)
     models = models_response.json()
-    print(models[0])
-    #Returns output like
-    #[{'id': 'USEEIOv1.1',
-    #  'name': 'USEEIOv1.1',
-    #  'location': 'US',
-    #  'description': 'EPA national life cycle model of goods and services, v1.1.'}]
+    model = models[0]
+
+    sectors_response = requests.get(
+        urljoin([base_url, model["id"], "sectors"]), headers=headers
+    )
+    sectors = sectors_response.json()
+
+    A0_response = requests.get(
+        urljoin([base_url, model["id"], "matrix", "A"]),
+        params={"col": 0},
+        headers=headers,
+    )
+
+    A0 = A0_response.json()
+    A0_df = pandas.DataFrame(A0, index=[sector["id"] for sector in sectors])
+    print(A0_df)

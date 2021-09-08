68aa6875b707075b709fe9a2bfde06fab8c5027a
abrie <abrie@users.noreply.github.com>
2021-09-07 20:37:43 -0400

    Add react-leaflet.

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,7 +1,22 @@
+const path = require("path");
+const babelInclude = require("@dealmore/craco-plugin-babel-include");
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
+  plugins: [
+    {
+      plugin: babelInclude,
+      options: {
+        include: [
+          path.resolve("src"),
+          path.resolve("../node_modules/@react-leaflet"),
+          path.resolve("../node_modules/react-leaflet"),
+        ],
+      },
+    },
+  ],
 };
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer } from "react-leaflet";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={[45.4, -75.7]} zoom={12}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
   );
 }
-
-export default App;

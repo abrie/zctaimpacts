5a85dd960c5024c1da4a587ff05c99a9956bcf97
abrie <abrie@users.noreply.github.com>
2021-09-09 14:36:25 -0400

    Refactor gis module.

--- backend/app/gis.py
+++ backend/app/gis.py
@@ -1,35 +1,47 @@
 import json
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
-from werkzeug.exceptions import abort
+from flask import Blueprint, request
 
 from app.db import get_db
 
 blueprint = Blueprint("gis", __name__, url_prefix="/gis")
 
 
+def buildQueryParams(json_data):
+    return {
+        "x1": json_data["x1"],
+        "y1": json_data["y1"],
+        "x2": json_data["x2"],
+        "y2": json_data["y2"],
+    }
+
+
+sql = """
+SELECT
+  zcta_geojson.ZCTA5CE20,
+  zcta_geojson.geometry
+FROM
+  zcta_geojson
+  INNER JOIN (
+    SELECT
+      ZCTA5CE20
+    FROM
+      zcta_shp
+    WHERE
+      MBRIntersects(BuildMBR(:x1,:y1,:x2,:y2, 4326), "geometry")
+  ) AS zcta_shp ON zcta_geojson.ZCTA5CE20 = zcta_shp.ZCTA5CE20
+"""
+
+
 @blueprint.route("/zcta", methods=["POST"])
 def zcta():
-    db = get_db()
-    json_data = request.get_json()
-
-    rows = db.execute(
-        """
-        SELECT
-          zcta_geojson.ZCTA5CE20,
-          zcta_geojson.geometry
-        FROM
-          zcta_geojson
-          INNER JOIN (
-            SELECT
-              ZCTA5CE20
-            FROM
-              zcta_shp
-            WHERE
-              MBRIntersects(BuildMBR(?, ?, ?, ?, 4326), "geometry")
-          ) AS zcta_shp ON zcta_geojson.ZCTA5CE20 = zcta_shp.ZCTA5CE20
-        """,
-        (json_data["x1"], json_data["y1"], json_data["x2"], json_data["y2"]),
-    ).fetchall()
+    rows = (
+        get_db()
+        .execute(
+            sql,
+            buildQueryParams(request.get_json()),
+        )
+        .fetchall()
+    )
 
     return {
         "results": [

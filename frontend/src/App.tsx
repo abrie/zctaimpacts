d4ae4c4b1cf2c484a2619269a36a62f084764175
abrie <abrie@users.noreply.github.com>
2021-09-09 18:29:40 -0400

    Refactor/clean up prototype code.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,45 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { MapContainer, TileLayer, GeoJSON, useMapEvents } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
+import { LatLngExpression, Map } from "leaflet";
 import axios from "axios";
 
 const center: LatLngExpression = [33.813534, -84.403339];
+const style = {
+  fillColor: "green",
+  weight: 2,
+  opacity: 0.5,
+  color: "#555",
+  dashArray: "1",
+  fillOpacity: 0.1,
+};
 export default function App() {
   const [layers, setLayers] = useState([]);
   const [status, setStatus] = useState("");
 
-  function MyComponent() {
+  async function loadVisibleZCTA(map: Map) {
+    const bounds = map.getBounds();
+    const data = {
+      x1: bounds.getWest(),
+      y1: bounds.getNorth(),
+      x2: bounds.getEast(),
+      y2: bounds.getSouth(),
+    };
+    setStatus("Loading zipcode areas...");
+    const response = await axios.post("/gis/zcta", data);
+    setLayers(response.data.results);
+    setStatus("");
+  }
+
+  function MapComponent() {
     const map = useMapEvents({
-      dragend: async () => {
-        const bounds = map.getBounds();
-        const data = {
-          x1: bounds.getWest(),
-          y1: bounds.getNorth(),
-          x2: bounds.getEast(),
-          y2: bounds.getSouth(),
-        };
-        setStatus("Loading zipcode areas...");
-        const response = await axios.post("/gis/zcta", data);
-        setLayers(response.data.results);
-        setStatus("");
+      resize: () => {
+        loadVisibleZCTA(map);
       },
-      click: () => {
-        map.locate();
+      zoomend: () => {
+        loadVisibleZCTA(map);
       },
-      locationfound: (location) => {
-        console.log("location found:", location);
+      dragend: () => {
+        loadVisibleZCTA(map);
       },
     });
     return null;
@@ -39,14 +51,15 @@ export default function App() {
         center={center}
         zoom={11}
         className="w-full h-full flex-grow"
+        whenCreated={(map) => loadVisibleZCTA(map)}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <MyComponent />
+        <MapComponent />
         {layers.map(({ zipcode, geometry }) => (
-          <GeoJSON key={zipcode} data={geometry} />
+          <GeoJSON key={zipcode} data={geometry} style={style} />
         ))}
       </MapContainer>
       <div className="flex-grow-0 h10 bg-gray-500 p-1 h-9 text-white">

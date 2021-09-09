f4d661678815dc8751caf77afdc6a4810de66416
abrie <abrie@users.noreply.github.com>
2021-09-08 20:16:44 -0400

    Use MBRIntersects to find visible ZCTAs.

--- geo/app/gis.py
+++ geo/app/gis.py
@@ -11,14 +11,10 @@ bp = Blueprint("gis", __name__, url_prefix="/gis")
 def zcta():
     db = get_db()
     json_data = request.get_json()
-    x1 = json_data["x1"]
-    y1 = json_data["y1"]
-    x2 = json_data["x2"]
-    y2 = json_data["y2"]
 
     rows = db.execute(
-        """SELECT zcta_geojson.ZCTA5CE20, zcta_geojson.geometry from zcta_geojson inner join (select ZCTA5CE20 from zcta_shp where MBRContains(BuildMBR(?,?,?,?, 4326), "geometry")) as zcta_shp ON zcta_geojson.ZCTA5CE20 = zcta_shp.ZCTA5CE20""",
-        (x1, y1, x2, y2),
+        """SELECT zcta_geojson.ZCTA5CE20, zcta_geojson.geometry from zcta_geojson inner join (select ZCTA5CE20 from zcta_shp where MBRIntersects(BuildMBR(?,?,?,?, 4326), "geometry")) as zcta_shp ON zcta_geojson.ZCTA5CE20 = zcta_shp.ZCTA5CE20""",
+        (json_data["x1"], json_data["y1"], json_data["x2"], json_data["y2"]),
     ).fetchall()
 
     return {

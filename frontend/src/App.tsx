aabf9f9860203b6b26d49b02a1abf4d6b9d7e28b
abrie <abrie@users.noreply.github.com>
2021-09-15 21:18:04 -0400

    Use black-and-white map.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,37 @@ import { LatLngExpression, LeafletEventHandlerFnMap, Map } from "leaflet";
 import { RiCollageLine } from "react-icons/ri";
 import axios from "axios";
 
+const providers = [
+  {
+    url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  },
+  {
+    url:
+      "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png",
+    attribution:
+      '&copy; Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+];
+
+const activeProvider = 0;
+
 const center: LatLngExpression = [33.813534, -84.403339];
 const style = {
   fillColor: "green",
-  weight: 2,
-  opacity: 0.2,
-  color: "black",
+  weight: 1,
+  opacity: 0.1,
+  color: "red",
   dashArray: "1",
-  fillOpacity: 0.1,
+  fillOpacity: 0,
 };
 
 const highlightStyle = {
   weight: 5,
-  color: "#666",
+  color: "lightgreen",
   dashArray: "",
-  fillOpacity: 0.7,
+  fillOpacity: 0.5,
 };
 
 export default function App() {
@@ -94,8 +110,8 @@ export default function App() {
         whenCreated={(map) => loadVisibleZCTA(map)}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url={providers[activeProvider].url}
+          attribution={providers[activeProvider].attribution}
         />
         <MapComponent />
         {layers.map(({ zipcode, geometry }) => (

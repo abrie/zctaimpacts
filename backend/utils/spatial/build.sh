85e498ec258035993c3e28bf93257da3449578ec
abrie <abrie@users.noreply.github.com>
2021-09-18 22:16:12 -0400

    Incorporate County GIS information.

--- backend/utils/spatial/build.sh
+++ backend/utils/spatial/build.sh
@@ -11,8 +11,8 @@ echo "SELECT CreateSpatialIndex('zcta_shp', 'geometry');" | spatialite out/$DB
 npx mapshaper raw/$ZCTA_SHAPEFILE.shp -simplify dp 10% -o force format=geojson out/$ZCTA_SHAPEFILE.json
 geojson-to-sqlite out/$DB zcta_geojson out/$ZCTA_SHAPEFILE.json
 
-npx mapshaper raw/$COUNTY_SHAPEFILE.shp -o force format=shapefile out/$COUNTY_SHAPEFILE.shp
+npx mapshaper raw/$COUNTY_SHAPEFILE.shp -simplify dp 2% -o force format=shapefile out/$COUNTY_SHAPEFILE.shp
 echo ".loadshp out/$COUNTY_SHAPEFILE county_shp UTF8 4326" | spatialite out/$DB
 echo "SELECT CreateSpatialIndex('county_shp', 'geometry');" | spatialite out/$DB
-npx mapshaper raw/$COUNTY_SHAPEFILE.shp -o force format=geojson out/$COUNTY_SHAPEFILE.json
+npx mapshaper raw/$COUNTY_SHAPEFILE.shp -simplify dp 10% -o force format=geojson out/$COUNTY_SHAPEFILE.json
 geojson-to-sqlite out/$DB county_geojson out/$COUNTY_SHAPEFILE.json

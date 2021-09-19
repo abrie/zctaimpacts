ea98807a8c15a2f18b578878254d35457a5ce089
abrie <abrie@users.noreply.github.com>
2021-09-19 09:35:55 -0400

    Only scrape used sheets instead of everything.

--- backend/app/useeio/matrices.py
+++ backend/app/useeio/matrices.py
@@ -11,13 +11,13 @@ def load_matrices():
     with pkg_resources.path(resources, "USEEIOv1.1_Matrices.xlsx") as filepath:
         with pandas.ExcelFile(filepath) as xls:
             return {
-                "A": pandas.read_excel(xls, "A", header=0, index_col=0),
-                "B": pandas.read_excel(xls, "B", header=0, index_col=0),
-                "C": pandas.read_excel(xls, "C", header=0, index_col=0),
+                # "A": pandas.read_excel(xls, "A", header=0, index_col=0),
+                # "B": pandas.read_excel(xls, "B", header=0, index_col=0),
+                # "C": pandas.read_excel(xls, "C", header=0, index_col=0),
                 "D": pandas.read_excel(xls, "D", header=0, index_col=0),
-                "L": pandas.read_excel(xls, "L", header=0, index_col=0),
-                "LCI": pandas.read_excel(xls, "LCI", header=0, index_col=0),
-                "U": pandas.read_excel(xls, "U", header=0, index_col=0),
+                # "L": pandas.read_excel(xls, "L", header=0, index_col=0),
+                # "LCI": pandas.read_excel(xls, "LCI", header=0, index_col=0),
+                # "U": pandas.read_excel(xls, "U", header=0, index_col=0),
             }
 
 

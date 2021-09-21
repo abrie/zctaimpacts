345b27a4630ce5d8851ba0af954f79c3f9b478f2
abrie <abrie@users.noreply.github.com>
2021-09-21 10:40:43 -0400

    Use flexible width for impact label.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -140,7 +140,7 @@ interface ImpactLineParams {
 function ImpactLine({ label, units, value }: ImpactLineParams): JSX.Element {
   return (
     <div className="flex flex-row border-b border-black mb-1 pb-1">
-      <div className="whitespace-nowrap text-xs font-bold w-24 truncate overflow-hidden overflow-ellipsis">
+      <div className="whitespace-nowrap text-xs font-bold max-w-sm truncate overflow-hidden overflow-ellipsis mr-1">
         {label}
       </div>
       <div className="text-xs whitespace-nowrap">({units})</div>

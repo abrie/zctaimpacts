82a891a344f3bf7c6898461109f0053119021c3d
abrie <abrie@users.noreply.github.com>
2021-09-25 21:57:12 -0400

    Use v2.0 USEEIO model.

--- backend/app/useeio/matrices.py
+++ backend/app/useeio/matrices.py
@@ -7,13 +7,16 @@ matrices = None
 def load_matrices() -> dict:
     from . import resources
 
-    with pkg_resources.path(resources, "USEEIOv1.1_Matrices.xlsx") as filepath:
+    with pkg_resources.path(resources, "USEEIOv2.0.xlsx") as filepath:
         with pandas.ExcelFile(filepath) as xls:
             return {
                 # "A": pandas.read_excel(xls, "A", header=0, index_col=0),
                 # "B": pandas.read_excel(xls, "B", header=0, index_col=0),
                 # "C": pandas.read_excel(xls, "C", header=0, index_col=0),
                 "D": pandas.read_excel(xls, "D", header=0, index_col=0),
+                "SectorCrosswalk": pandas.read_excel(
+                    xls, "SectorCrosswalk", header=0
+                )
                 # "L": pandas.read_excel(xls, "L", header=0, index_col=0),
                 # "LCI": pandas.read_excel(xls, "LCI", header=0, index_col=0),
                 # "U": pandas.read_excel(xls, "U", header=0, index_col=0),

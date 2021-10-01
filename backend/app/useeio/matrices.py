fa64eb5f077beba6f62aa172337897c28cc9f5a4
abrie <abrie@users.noreply.github.com>
2021-10-01 13:58:09 -0400

    Remove commented code.

--- backend/app/useeio/matrices.py
+++ backend/app/useeio/matrices.py
@@ -10,16 +10,11 @@ def load_matrices() -> dict:
     with pkg_resources.path(resources, "USEEIOv2.0.xlsx") as filepath:
         with pandas.ExcelFile(filepath) as xls:
             return {
-                # "A": pandas.read_excel(xls, "A", header=0, index_col=0),
-                # "B": pandas.read_excel(xls, "B", header=0, index_col=0),
-                # "C": pandas.read_excel(xls, "C", header=0, index_col=0),
                 "D": pandas.read_excel(xls, "D", header=0, index_col=0),
                 "SectorCrosswalk": pandas.read_excel(xls, "SectorCrosswalk", header=0),
                 "indicators": pandas.read_excel(
                     xls, "indicators", header=0, index_col=0
                 ),
-                # "LCI": pandas.read_excel(xls, "LCI", header=0, index_col=0),
-                # "U": pandas.read_excel(xls, "U", header=0, index_col=0),
             }
 
 
